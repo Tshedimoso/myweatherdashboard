6bb66076d3b035277bfdb4e904a293fa3076b39e
Tshedimoso Segona <segonatshedimoso03@gmail.com>
2025-03-26 19:25:58 +0200

    Initial commit: Added my weather dashboard app with background image

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App(){
+  return(<>
+    
+    <div className="h-screen bg-landing">
+  
+  
+ </div>
+
+ <div className="bg-blue-300 h-24">
+
+
+ </div>
+
+ </>);
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'landing':"url('src/bg.image.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
