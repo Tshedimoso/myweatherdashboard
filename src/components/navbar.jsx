ec219d485e8ff5db860ad394a0c3b0710692ce7e
Tshedimoso Segona <segonatshedimoso03@gmail.com>
2025-04-06 10:19:04 +0200

    editing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import WithName from "../logo2";
 import Buttons from "./option_buttons";
-/*import MenuIcon from "./Icons/magnifying-glass";
-import SearchBox from "./search_box";*/
+
 function Navbar(){
     return (
         
     <div className="p-4 flex justify-between " >
          <WithName/>
         <Buttons/>
+        
 </div>);
 
 
